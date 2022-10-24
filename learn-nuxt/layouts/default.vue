9eb61e215aaac5fddf4a53ab499f0351e3ca63bc
hjun-park <phj0860@gmail.com>
2022-10-24 10:50:07 +0900

    레이아웃 컴포넌트

--- learn-nuxt/layouts/default.vue
+++ learn-nuxt/layouts/default.vue
@@ -2,8 +2,10 @@
   <!-- 레이아웃 -> 페이지 -> 공통 컴포넌트 렌더링 된다고 보면 됨 -->
   <!-- $route 객체 이름 전달-->
   <div>
+    <header>
     <h1>{{ $route.name }} 페이지</h1>
-    <Nuxt />
+  </header>
+  <Nuxt />
   </div>
 
   
@@ -12,7 +14,7 @@
 <script>
 export default {
   created() {
-    console.log(this.$route)
+    // console.log(this.$route)
   },
 }
 </script>
