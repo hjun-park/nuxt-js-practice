448bd8d51902aae64c569da1c4d53513fc7ebea4
hjun-park <phj0860@gmail.com>
2022-10-26 14:32:25 +0900

    axios 설치 및 API 호출 [상세내용 참고]
    1. /backend 내에서 npm run dev --> node 서버
    2. root 경로인 /learn-nuxt에서 npm run dev --> nuxt 서버

--- learn-nuxt/pages/main.vue
+++ learn-nuxt/pages/main.vue
@@ -5,9 +5,17 @@
 </template>
 
 <script>
-  export default {
-    
+// axios 이슈 [조치방법: https://www.inflearn.com/questions/672613]
+import axios from 'axios';
+
+export default {
+  // created를 통해서 products Main을 그림
+  // 이렇게 되면 노드서버 (/backend) 내의 products 데이터들을 들고옴
+  async created() {
+    const response = await axios.get('http://localhost:3000/products')
+    console.log(response)
   }
+}
 </script>
 
 <style lang="scss" scoped>
