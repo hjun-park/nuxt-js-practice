ef0e9bb398a108f9351f2523b0f5cbd98efba954
hjun-park <phj0860@gmail.com>
2022-10-31 18:37:24 +0900

    서버 포트 변경 및 받아온 데이터 표시

--- learn-nuxt/pages/main.vue
+++ learn-nuxt/pages/main.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <p>메인 페이지입니다</p>
+    <div>
+      {{ products }}
+    </div>
   </div>
 </template>
 
@@ -9,11 +12,18 @@
 import axios from 'axios';
 
 export default {
+  data() {
+    return {
+      products: [],
+    }
+  },
+
   // created를 통해서 products Main을 그림
   // 이렇게 되면 노드서버 (/backend) 내의 products 데이터들을 들고옴
   async created() {
     const response = await axios.get('http://localhost:3000/products')
     console.log(response)
+    this.products = response.data
   }
 }
 </script>
